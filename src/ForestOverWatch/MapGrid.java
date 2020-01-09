7ba0f1242b4017853d0300dddfcdb5d42c78c479
Simão Amorim <simao.amorim@fe.up.pt>
2020-01-09 12:27:50 +0100

    Initialization for MapGrid
    Probably there is no need to use neighbours because this will only be used as a visualization window, not used for computations

--- src/ForestOverWatch/MapGrid.java
+++ src/ForestOverWatch/MapGrid.java
@@ -1,9 +1,11 @@
 package ForestOverWatch;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.logging.Logger;
 
-public class MapGrid {
-    private int cellSize = 10;
+public class MapGrid extends JComponent {
+    private int cellSize;
     private static final int margin = 1;
     public static Integer[] Sizes = {50,100,200,500,1000};
     private int XCount;
@@ -11,11 +13,24 @@ public class MapGrid {
     private TerrainPoint[][] terrainPoints;
     private Logger logger;
 
-    MapGrid(int XCount, int YCount, Logger logger) {
+    MapGrid(int XCount, int YCount, int cellSIze, Logger logger) {
         this.XCount = XCount;
         this.YCount = YCount;
+        this.cellSize = cellSIze;
         this.logger = logger;
+        this.setPreferredSize(
+                new Dimension(
+                        (XCount *cellSize)+(2*margin),
+                        (YCount *cellSize)+(2*margin)
+                )
+        );
+    }
 
+    void initialize() {
+        terrainPoints = new TerrainPoint[XCount][YCount];
+        for (int x = 0; x < XCount; x++)
+            for (int y = 0; y < YCount; y++)
+                terrainPoints[x][y] = new TerrainPoint();
     }
 
 }
